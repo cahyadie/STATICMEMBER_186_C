cdc3f45b04d2b43e6b25ffe36bd29462bf06a586
cahyadie <cahyadiilham12@gmail.com>
2023-05-15 11:26:56 +0700

    make class mahasiswa and put static on it

--- STATICMEMBER_186/STATICMEMBER_186.cpp
+++ STATICMEMBER_186/STATICMEMBER_186.cpp
@@ -2,3 +2,13 @@
 #include <string>
 using namespace std;
 
+class mahasiswa {
+public:
+	static int nim;
+	int id;
+	string nama;
+
+	void setID();
+	void printAI();
+	mahasiswa(string pnama) :nama(pnama) { setID(); }
+};
